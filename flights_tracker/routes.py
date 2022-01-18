2e2c45bdf1429808e0862ecfd001fd0c122a23f7
Rafał Stępień <rafal.stepien@stxnext.pl>
2022-01-18 20:33:01 +0100

    fix(flights_tracker/routes.py): update endpoint address

--- flights_tracker/routes.py
+++ flights_tracker/routes.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Request, Response
 router = APIRouter()
 
 
-@router.get("/weekend_flights")
+@router.get("/weekend-flights")
 def get_weekend_flights(http_request: Request):
     app = http_request.app
     message = app.weekend_flights_service.process()
